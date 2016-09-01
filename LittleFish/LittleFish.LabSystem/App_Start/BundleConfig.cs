b242b4e1c57635c1986fb6f42715211c08d9b469
xiaoyufish <784571419@qq.com>
2016-09-01 17:39:23 +0800

    用easyui设计管理后台主页面

--- LittleFish/LittleFish.LabSystem/App_Start/BundleConfig.cs
+++ LittleFish/LittleFish.LabSystem/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace LittleFish.LabSystem
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.easyui-{version}.js"));
 
             // 使用要用于开发和学习的 Modernizr 的开发版本。然后，当你做好
             // 生产准备时，请使用 http://modernizr.com 上的生成工具来仅选择所需的测试。
@@ -22,6 +23,7 @@ namespace LittleFish.LabSystem
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/themes/default/easyui.css",
                       "~/Content/site.css"));
         }
     }
