1343eddf24fa71cd8db49a49d360d94de7c2e93c
xiaoyufish <784571419@qq.com>
2016-09-13 15:44:48 +0800

    使用NFine框架样式重新设计主页面，需求使用文档http://wenku.baidu.com/link?url=Y9_q_994Nd7n2ZHU0oX4-2aNQpMlNS93N_OECeS3GwtWDY3HmCzXLIAq6KuZVtKJPW5dAo6fqw2-FecEwHHXo1l_YTF3iNPmqeTCNbEmKEy

--- LittleFish/LittleFish.LabSystem/App_Start/BundleConfig.cs
+++ LittleFish/LittleFish.LabSystem/App_Start/BundleConfig.cs
@@ -9,22 +9,14 @@ namespace LittleFish.LabSystem
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/jquery.easyui-{version}.js"));
-
-            // 使用要用于开发和学习的 Modernizr 的开发版本。然后，当你做好
-            // 生产准备时，请使用 http://modernizr.com 上的生成工具来仅选择所需的测试。
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/themes/default/easyui.css",
-                      "~/Content/site.css"));
+                      "~/Content/app-theme.css"));
         }
     }
 }
