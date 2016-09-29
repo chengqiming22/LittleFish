67c447ce143bbd5808c3dc91612a925bc065d933
xiaoyufish <784571419@qq.com>
2016-09-29 10:40:53 +0800

    添加基本功能模块

---
+++ LittleFish/LittleFish.LabSystem/Controllers/InstrumentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LittleFish.LabSystem.Controllers
+{
+    public class InstrumentController : Controller
+    {
+        // GET: Instrument
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
